f16683ca15e83028fdd795211db5ccfa8441133c
Didier <europrimus@free.fr>
2018-05-14 14:41:05 +0200

    initialisation

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr" prefix="og: http://ogp.me/ns#">
+<!--
+   Copyright 2018 Didier <didier@courty.fr>
+-->
+
+<head>
+<meta charset="utf-8">
+<title>Morpion</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Feuille de style -->
+	<link rel="stylesheet" href="css/style.css">
+
+<!-- Open Graph (http://ogp.me/) -->
+	<meta property="og:title" content="sans titre">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="">
+	<meta property="og:image" content="">
+</head>
+
+<body>
+	<header>
+    <h1>Morpion</h1>
+	</header>
+
+	<main>
+    <section>
+
+    </section>
+	</main>
+
+	<footer>
+	</footer>
+
+</body>
+
+<!-- Les script JavaScript -->
+
+</html>
---
+++ serveur.php
@@ -0,0 +1,19 @@
+<?php
+// initialise une grille vide
+$grille=[[[],[],[]],[[],[],[]],[[],[],[]]];
+echo "grille:<pre>";var_dump($grille);echo "</pre>";
+
+// attend action J1
+// coordonnés de la case joué
+
+// renvois info à J2
+// toute la grille
+
+
+// attend action J2
+// coordonnés de la case
+
+// renvois info à J1
+// toute la grille
+
+?>
