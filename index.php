3b0ba6312eb9eb0bdb4536d56947ff8105ffe038
Didier <europrimus@free.fr>
2018-05-14 15:58:17 +0200

    fichier de la partie

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <title>Morpion</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- Feuille de style -->
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="style.css">
 
 <!-- Open Graph (http://ogp.me/) -->
 	<meta property="og:title" content="sans titre">
@@ -21,6 +21,13 @@
 <body>
 	<header>
     <h1>Morpion</h1>
+Exercice Ajax/POO : morpion
+		<ul>
+			<li>Créez un serveur de morpion qui permet de se connecter au serveur, renseigner son nom et attendre (ou pas) l'arrivée d'un adversaire</li>
+			<li>Les joueurs jouent alors tour à tour</li>
+			<li>Un joueur ne doit pas pouvoir jouer quand ce n'est pas son tour</li>
+			<li>Pour faire persister les données, utilisez un fichier représentant l'état de la partie</li>
+</ul>
 	</header>
 
 	<main>
--- serveur.php
+++ serveur.php
@@ -1,10 +1,38 @@
 <?php
+/*
+ressources:
+https://developer.mozilla.org/en-US/docs/Web/API/EventSource/EventSource
+https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
+
+depot github:
+https://github.com/europrimus/WF3_Morpion
+*/
+
+// initialisation de la session
+session_start();
+
+$fichierPartie="partie.json";
+
+if( is_file( $fichierPartie ) ){
+  $partie = json_decode( file_get_contents( $fichierPartie ) , true );
+  echo "lecture du fichier $fichierPartie :<pre>";var_dump( $partie );echo "</pre>";
+};
+
+
+// si pas des grille
+if( !isset($partie["grille"]) ){
 // initialise une grille vide
-$grille=[[[],[],[]],[[],[],[]],[[],[],[]]];
-echo "grille:<pre>";var_dump($grille);echo "</pre>";
+  $partie["grille"]=[["","",""],["","",""],["","",""]];
+  echo "grille:<pre>";var_dump( $grille );echo "</pre>";
+};
 
 // attend action J1
 // coordonnés de la case joué
+if( isset($_REQUEST["case"]) ){
+  echo "le joueur ".$_REQUEST["joueur"]." à joué la case ".$_REQUEST["case"]."<br>".PHP_EOL;
+  $case=str_split ( $_REQUEST["case"] , 1);
+  $partie["grille"][$case[0]][$case[1]]=$_REQUEST["joueur"];
+};
 
 // renvois info à J2
 // toute la grille
@@ -15,5 +43,10 @@ echo "grille:<pre>";var_dump($grille);echo "</pre>";
 
 // renvois info à J1
 // toute la grille
+$json=json_encode($partie);
+$fichier = fopen ( $fichierPartie , "w" );
+fwrite ( $fichier , $json );
+fclose( $fichier );
 
+echo $json;
 ?>
